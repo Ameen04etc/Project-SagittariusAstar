bbba73a5248255f9a8f0aa590d87d2ee7bb48689
Ameen Aazam <ameenaazam04@gmail.com>
2026-08-24 21:16:46 +0530

    anNaylam: Update

--- anNaylam.py
+++ anNaylam.py
@@ -5,7 +5,7 @@ from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget,
                                QGridLayout, QScrollBar, QSizePolicy,
                                QPushButton, QToolButton, QToolTip,
                                QFrame, QLabel, QTreeView,
-                               QPlainTextEdit, QTextEdit)
+                               QPlainTextEdit, QTextEdit, QFileDialog)
 from PySide6.QtCore import    (QProcess, Qt, QObject,
                                Signal, QRectF, QRect,
                                Slot, QPointF, QPoint,
@@ -16,9 +16,11 @@ from PySide6.QtGui import     (QPainter, QColor, QPen,
                                QImage, QCursor, QPainterPath,
                                QStandardItemModel, QStandardItem,
                                QFontMetrics, QKeySequence, QTextFormat,
-                               QTextCursor, QTextBlock)
+                               QTextCursor, QTextBlock, QShortcut,
+                               QTextCharFormat)
 from enum import Enum, auto
 from typing import cast
+from pathlib import Path
 import json
 import os
 import sys
@@ -35,6 +37,18 @@ YELLOW = "\033[33m"
 BLUE = "\033[34m"
 RESET = "\033[0m"
 
+LANGUAGE_MAP = {
+    ".py"   : "python",
+    ".js"   : "javascript",
+    ".ts"   : "typescript",
+    ".html" : "html",
+    ".css"  : "css",
+    ".json" : "json",
+    ".cpp"  : "cpp",
+    ".c"    : "c"
+}
+
+
 class LineIndent(Enum):
     Indent = auto()
     Dedent = auto()
@@ -46,8 +60,9 @@ class MasterWidget(QWidget):
     def __init__(self, parent):
         super().__init__(parent)
         self.editor = CodeEditor(self)
-        self.Client = LSPClient()
+        self.client = LSPClient()
         self.LayoutConfig()
+        self.SignalManager()
 
     def LayoutConfig(self):
         Layout = QHBoxLayout(self)
@@ -55,6 +70,22 @@ class MasterWidget(QWidget):
         Layout.setSpacing(0)
         Layout.addWidget(self.editor.LineWidget)
         Layout.addWidget(self.editor)
+
+    def LSPDocConfig(self):
+        self.client.Document.uri        = self.editor.FilePath
+        self.client.Document.languageId = LANGUAGE_MAP.get(self.editor.FileExt.lower(), "python")
+        self.client.Document.text       = self.editor.toPlainText()
+        if self.editor.NewFile:
+            self.client.Document.version = 1
+            self.editor.NewFile = False
+            self.client.didOpenMessage()
+        else:
+            self.client.Document.version += 1
+            self.client.didChangeMessage()
+
+    def SignalManager(self):
+        self.editor.textChanged.connect(self.LSPDocConfig)
+        self.client.diagnosticsReady.connect(self.editor.diagnose)
 
 
 class LineNumberArea(QWidget):
@@ -123,12 +154,22 @@ class LineNumberArea(QWidget):
 
 
 class CodeEditor(QPlainTextEdit):
+    fileOpened = Signal()
 
     def __init__(self, parent = None):
         super().__init__(parent)
         self.Language  = PythonLanguage()
-
+        self.version   = 1
+        self.FilePath  = None
+        self.FileExt   = "plaintext"
+        self.NewFile   = False
         self.Selection = CodeSelection()
+
+        self.errSelections = []
+        self.errSquiggles  : list[Squiggle] = []
+        self.warnSquiggles : list[Squiggle] = []
+
+        self.Diagnostics = {}
 
         self.Font = QFont()
         self.Font.setFamilies(["Consolas", "Courier New"])
@@ -153,7 +194,74 @@ class CodeEditor(QPlainTextEdit):
         self.StyleConfig()
         self.HighLightLine()
         self.SignalManager()
-        self.selectionChanged.connect(self.updateSelection)
+
+    def paintEvent(self, e):
+        painter = QPainter(self.viewport())
+        painter.fillRect(e.rect(), QColor(20, 20, 20))
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+
+        painter.setPen(QPen(QColor(205, 49, 49), 1.5))
+        for squiggle in self.errSquiggles: self.drawSquiggle(painter, squiggle)
+
+        painter.setPen(QPen(QColor(229, 229, 16), 1.5))
+        for squiggle in self.warnSquiggles: self.drawSquiggle(painter, squiggle)
+
+        super().paintEvent(e)
+
+    def drawSquiggle(self, painter : QPainter, squiggle : Squiggle):
+        for line in range(squiggle.start.row, squiggle.end.row + 1):
+            block = self.document().findBlockByNumber(line)
+            if not block.isValid():
+                continue
+
+            blockLen = max(0, block.length() - 1)
+            if line == squiggle.start.row:
+                colStart = min(squiggle.start.col, blockLen)
+                cursor = QTextCursor(block)
+                cursor.setPosition(block.position() + colStart)
+                startRect = self.cursorRect(cursor)
+                x0 = startRect.x()
+            else:
+                cursor = QTextCursor(block)
+                cursor.setPosition(block.position())
+                startRect = self.cursorRect(cursor)
+                x0 = startRect.x()
+
+            if line == squiggle.end.row:
+                if line > squiggle.start.row and squiggle.end.col == 0:
+                    continue
+                colEnd = min(squiggle.end.col, blockLen)
+                cursor = QTextCursor(block)
+                cursor.setPosition(block.position() + colEnd)
+                endRect = self.cursorRect(cursor)
+                x1 = endRect.x()
+            else:
+                cursor = QTextCursor(block)
+                cursor.setPosition(block.position() + block.length() - 1)
+                endRect = self.cursorRect(cursor)
+                x1 = endRect.right()
+            
+            if x0 == x1: 
+                x0 -= self.cellWidth // 2
+                x1 += self.cellWidth // 2
+
+            yoff = startRect.bottom()
+            locus = QPainterPath()
+            locus.moveTo(x0, yoff)
+
+            A = 1.4
+            l = 6
+
+            for x in range(int(x0), int(x1) + 1):
+                y = A * math.sin((x) * (2 * math.pi / l)) + yoff
+                locus.lineTo(x, y)
+
+            painter.drawPath(locus)
+
+    def reportChange(self):
+        print("changed")
+        if self.NewFile: self.version = 1
+        else: self.version += 1
 
     def HighLightLine(self):
         line_color = QColor(255, 255, 255, 15)
@@ -164,7 +272,8 @@ class CodeEditor(QPlainTextEdit):
 
         selection.cursor = self.textCursor()
         selection.cursor.clearSelection()
-        self.setExtraSelections([selection])
+        self.setExtraSelections([selection] + self.errSelections)
+        # self.setExtraSelections(self.errSelections)
 
     def BlockIndent(self):
         cursor = self.textCursor()
@@ -276,18 +385,67 @@ class CodeEditor(QPlainTextEdit):
         #       self.Selection.FirstBlock.blockNumber(), "\n",
         #       self.Selection.LastBlock.blockNumber())
 
+    def openFile(self):
+        filepath, _ = QFileDialog.getOpenFileName(
+            self,
+            "Open File",
+            "",
+            "All Files (*);;Python Files (*.py)"
+        )
+
+        if filepath:
+            self.NewFile = True
+            self.FilePath   = Path(filepath).as_uri()
+            _, self.FileExt = os.path.splitext(filepath)    # Splits "C:/scripts/main.py" into ("C:/scripts/main", ".py")
+            with open(filepath, "r", encoding = "utf-8") as file:
+                text = file.read()
+            self.setPlainText(text)
+
+    def diagnose(self, uri, version, diagnostics : dict):
+        if version != self.version:
+            return
+        if uri != self.FilePath:
+            return
+        
+        self.Diagnostics = diagnostics
+        self.errSquiggles.clear()
+        self.warnSquiggles.clear()
+
+        for  diagnostic in self.Diagnostics:
+            if "range" in diagnostic.keys():
+                if diagnostic["severity"] == 1:
+                    err = Squiggle()
+                    err.start.row = diagnostic["range"]["start"]["line"]
+                    err.start.col = diagnostic["range"]["start"]["character"]
+                    err.end.row = diagnostic["range"]["end"]["line"]
+                    err.end.col = diagnostic["range"]["end"]["character"]
+                    self.errSquiggles.append(err)
+                elif diagnostic["severity"] == 2:
+                    warn = Squiggle()
+                    warn.start.row = diagnostic["range"]["start"]["line"]
+                    warn.start.col = diagnostic["range"]["start"]["character"]
+                    warn.end.row = diagnostic["range"]["end"]["line"]
+                    warn.end.col = diagnostic["range"]["end"]["character"]
+                    self.warnSquiggles.append(warn)
+        self.viewport().update()
+
     def SignalManager(self):
+        self.fileOpenShortcut = QShortcut(QKeySequence("Ctrl+O"), self)
+
+        self.fileOpenShortcut.activated.connect(self.openFile)
         self.blockCountChanged.connect(self.updateLineData)
         self.cursorPositionChanged.connect(self.updateLineData)
         self.cursorPositionChanged.connect(self.HighLightLine)
         self.updateRequest.connect(self.LineWidget.update)
+        self.selectionChanged.connect(self.updateSelection)
+        self.textChanged.connect(self.reportChange)
 
     def StyleConfig(self):
         self.setStyleSheet(
             """
             QPlainTextEdit {
                 border: none;
-                background-color: rgb(20, 20, 20);
+                background-color: transparent;
                 selection-background-color: rgba(17, 168, 225, 100);
             }
             """
@@ -350,6 +508,18 @@ class CodeSelection:
         self.LastBlock  = None
 
 
+class Squiggle:
+    def __init__(self):
+        self.start = Coordinate()
+        self.end   = Coordinate()
+
+
+class Coordinate:
+    def __init__(self, lineId = 0, chId = 0):
+        self.row = lineId
+        self.col = chId
+
+
 class codeLanguage:
     def nextIndentation(self, cursor : QTextCursor):
         raise NotImplementedError
@@ -397,6 +567,7 @@ class readBuffer:
 
 
 class LSPClient(QObject):
+    diagnosticsReady = Signal(object, object, object)
 
     def __init__(self, parent = None):
         super().__init__(parent)
@@ -404,6 +575,7 @@ class LSPClient(QObject):
         self.OutputBuffer = readBuffer()
         self.BodyLength   = 0
         self.ReceivedLen  = 0
+        self.Document     = LSPDocument(None, "python", "")
 
         self.process.readyReadStandardOutput.connect(self.readOutput)
         self.process.readyReadStandardError.connect(self.readError)
@@ -459,6 +631,41 @@ class LSPClient(QObject):
 
         self.process.write(header + body)
 
+    def didOpenMessage(self):
+        message = {
+            "jsonrpc"   : "2.0",
+            "method"    : "textDocument/didOpen",
+            "params"    : {
+                "textDocument"  : {
+                    "uri"           : self.Document.uri,
+                    "languageId"    : self.Document.languageId,
+                    "version"       : self.Document.version,
+                    "text"          : self.Document.text
+                }
+            }
+        }
+
+        self.sendMessage(message)
+
+    def didChangeMessage(self):
+        message = {
+            "jsonrpc"   : "2.0",
+            "method"    : "textDocument/didChange",
+            "params"    : {
+                "textDocument"  : {
+                    "uri"       : self.Document.uri,
+                    "version"   : self.Document.version
+                },
+                "contentChanges": [
+                    {
+                        "text"  : self.Document.text
+                    }
+                ]
+            }
+        }
+
+        self.sendMessage(message)
+
     def readOutput(self):
         data = bytes(self.process.readAllStandardOutput())
         self.OutputBuffer.Buffer += data
@@ -466,10 +673,10 @@ class LSPClient(QObject):
         self.checkBuffer()
 
     def checkBuffer(self):
-        print("----------")
+        # print("----------")
         # print("Buffer State =", self.OutputBuffer.State, "\r\n")
-        print(self.OutputBuffer.Buffer.decode("utf-8"), "\r\n\r\n")
-        print("----------")
+        # print(self.OutputBuffer.Buffer.decode("utf-8"), "\r\n\r\n")
+        # print("----------")
         while True:
             if self.OutputBuffer.State == readBufferState.HEADER:
                 headerEnd = self.OutputBuffer.Buffer.find(b"\r\n\r\n")
@@ -498,6 +705,7 @@ class LSPClient(QObject):
                 else: break
 
     def readMessage(self, message):
+        message = json.loads(message)
         if "id" in message:
             if "result" in message:
                 self.handleResponse(message)
@@ -511,13 +719,44 @@ class LSPClient(QObject):
         print("Error:\r\n", message, "\r\n\r\n")
 
     def handleResponse(self, message):
-        print("Response:\r\n", message, "\r\n\r\n")
+        # print("Response:\r\n", message, "\r\n\r\n")
+        pass
 
     def handleNotification(self, message):
-        print("Notification:\r\n", message, "\r\n\r\n")
+        # print("Notification:\r\n", message, "\r\n\r\n")
+        if message["method"] == "textDocument/publishDiagnostics":
+            self.handleDiagnostics(message)
+
+    def handleDiagnostics(self, message):
+        colorMap = {
+            0   : f"{RED}",
+            1   : f"{GREEN}",
+            2   : f"{YELLOW}",
+            3   : f"{BLUE}"
+        }
+        params  = message["params"]
+        uri     = params["uri"]
+        version = params["version"]
+        diagnostics = params["diagnostics"]
+
+        self.diagnosticsReady.emit(uri, version, diagnostics)
+
+        for i, diagnostic in enumerate(diagnostics):
+            k = i % 4
+            print(colorMap[k], diagnostic)
+            print(f"{RESET}")
+        pass
 
     def readError(self):
         print("Error")
+
+
+class LSPDocument:
+    def __init__(self, uri, languageId, text):
+        self.uri        = uri
+        self.languageId = languageId
+        self.text       = text
+        self.version    = 1
 
 
 class  MainWindow(QMainWindow):
@@ -526,6 +765,7 @@ class  MainWindow(QMainWindow):
         Main = MasterWidget(self)
         self.setWindowTitle("anNaylam")
         self.setCentralWidget(Main)
+
 
 app = QApplication([])
 window = MainWindow()
